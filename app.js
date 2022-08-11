c61cd2b8d67adb4ee0d27ec2f1afa45a2649cca5
Adam A <adamaubry2362@gmail.com>
2022-08-11 09:29:50 -0500

    connected mongoose to mongo

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Creativespot = require('./models/creativespot');
+
+mongoose.connect('mongodb://localhost:27017/creative-spot',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error:"))
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req,res) => {
     res.send('home')
+})
+
+app.get('/makecreative', async (req, res) => {
+    const spot = new Creativespot({title: 'My house', description: 'the start'});
+    await spot.save();
+    res.send(spot)
 })
 
 
